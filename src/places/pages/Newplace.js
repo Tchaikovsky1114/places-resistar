031a58e04d9b33c69f769cf606e4423257ea49d0
Kim Myungseong <forwarm5891@gmail.com>
2022-06-18 15:05:33 +0900

    feat: implament resister place page

--- src/places/pages/Newplace.js
+++ src/places/pages/Newplace.js
@@ -1,11 +1,54 @@
-import React from 'react';
+import React, { useCallback, useReducer } from 'react';
+import {VALIDATOR_MIN } from '../../shared/util/validators';
 import Input from '../../shared/components/FormElements/Input';
+import { VALIDATOR_REQUIRE } from '../../shared/util/validators';
+
+const formReducer = (state,action) => {
+
+}
 
 const Newplace = () => {
+
+  const [formState,dispatch] = useReducer(formReducer,{
+    inputs: {
+      title: {
+        value:'',
+        isValid:false
+      },
+      description:{
+        value:'',
+        isValid:false
+      }
+    },
+    isValid:''
+  })
+
+  const titleInputHandler = useCallback((id, value,isValid) => {
+  },[])
+
+  const descriptionInputHandler = useCallback((id, value,isValid) => {
+
+  },[])
   return (
-    <form className='list-none my-0 m-auto p-4 w-11/12 max-w-2xl shadow-md rounded-md bg-white'>
-    
-    <Input element="input" type="text" label="Title" />
+    <form className="list-none my-0 m-auto p-4 w-11/12 max-w-2xl shadow-md rounded-md bg-white">
+      <Input
+        id='title'
+        element="input"
+        type="text"
+        label="Title"
+        validators={[VALIDATOR_REQUIRE()]}
+        errorText="제목은 필수 입력사항입니다!"
+        onChange={titleInputHandler}
+      />
+      <Input
+       id='description'
+        element="textarea"
+        type="text"
+        label="Description"
+        validators={[VALIDATOR_MIN(20)]}
+        errorText="최소 20자 이상 작성하셔야 해요"
+        onChange={titleInputHandler}
+      />
     </form>
   );
 };
--- src/shared/components/FormElements/Input.js
+++ src/shared/components/FormElements/Input.js
@@ -1,30 +1,84 @@
-import React from 'react';
+import React, { useEffect, useReducer } from 'react';
+import { validate } from '../../util/validators';
+
+const inputReducer = (state, action) => {
+  switch (action.type) {
+    case 'CHANGE':
+      return {
+        ...state,
+        value: action.payload,
+        isValid: validate(action.payload, action.validators),
+      };
+      case 'BLUR':
+        return {
+          ...state,
+          isBlur: true
+        }
+    default:
+      return state;
+  }
+};
 
 const Input = (props) => {
+  const [inputState, dispatch] = useReducer(inputReducer, {
+    value: '',
+    isValid: true,
+    isBlur:false,
+  });
+  const {id, onChange} = props;
+  const {value, isValid} = inputState;
+  useEffect(()=>{
+    onChange(props.id,value,isValid)
+  },[id,isValid,onChange])
+
+  // const onChange = (e) => {
+  //   dispatch({
+  //     type: 'CHANGE',
+  //     payload: e.target.value,
+  //     validators: props.validators,
+  //   });
+  // };
+  const onBlur = () => {
+    dispatch({
+      type: 'BLUR'
+    })
+  }
   const element =
     props.element === 'input' ? (
       <input
         className={`block w-full border boder-slate-200 bg-[#f8f8f8] py-1 px-2 focus:outline-none focus:bg-[#ebebeb] focus:border focus:border-purple-800
-        ${props.invalid && 'text-red-600 bg-[#ffd1d1]'}`}
+        `}
         id={props.id}
         type={props.type}
         placeholder={props.placeholder}
+        value={inputState.value}
+        onChange={onChange}
+        onBlur={onBlur}
       />
     ) : (
       <textarea
         className={`block w-full border boder-slate-200 bg-[#f8f8f8] py-1 px-2 focus:outline-none focus:bg-[#ebebeb] focus:border focus:border-purple-800
-        ${props.invalid && 'text-red-600 bg-[#ffd1d1]'}`}
+        `}
         id={props.id}
         rows={props.rows || 3}
+        value={inputState.value}
+        onChange={onChange}
+
       />
     );
   return (
-    <div className={`my-4`}>
-      <label className={`block font-bold mb-2
-      ${props.invalid && 'text-red-600'}`} htmlFor={props.id}>
+    <div
+      className={`my-4 ${!inputState.isValid && inputState.isBlur && 'text-red-600 bg-[#ffd1d1]'}`}
+    >
+      <label
+        className={`block font-bold mb-2
+      ${props.invalid && 'text-red-600'}`}
+        htmlFor={props.id}
+      >
         {props.label}
       </label>
       {element}
+      {!inputState.isValid && <p>{props.errorText}</p>}
     </div>
   );
 };
